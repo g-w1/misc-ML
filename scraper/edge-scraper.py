8a27e142da6373f67ab1450e397a0263d02b2bcc
g-w1 <58830309+g-w1@users.noreply.github.com>
2020-01-13 11:03:04 -0500

    Update edge-scraper.py

--- scraper/edge-scraper.py
+++ scraper/edge-scraper.py
@@ -2,8 +2,11 @@ from bs4 import BeautifulSoup
 import requests
 post = 'https://www.edge-ma.com/wp-login.php?action=postpass'
 after = 'https://www.edge-ma.com/student-area/curriculum/'
-payload = {"post-password":"DobbsFerry"}
+payload = {"post_password":"DobbsFerry","Submit":"Enter"}
 with requests.Session() as session:
     post = session.post(post, data=payload)
     r = session.get(after)
     soup = BeautifulSoup(r.content, 'html.parser')
+    a = soup.find_all("div",class_ = "btn-align-center")
+    print(a)
+
