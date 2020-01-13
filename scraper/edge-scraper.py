ffce1f3c2368bf7bad92fbb8dd777da5785e136b
g-w1 <58830309+g-w1@users.noreply.github.com>
2020-01-13 11:36:13 -0500

    Update edge-scraper.py

--- scraper/edge-scraper.py
+++ scraper/edge-scraper.py
@@ -8,7 +8,22 @@ with requests.Session() as session:
     r = session.get(after)
     soup = BeautifulSoup(r.content, 'html.parser')
     a = soup.find_all("div",class_ = "btn-align-center")
+    belts = {}
     for belt in a:
-      print(belt.a.text)
+      print(belt.a.text.lower())
       print("https://www.edge-ma.com/student-area/curriculum/"+belt.a["href"])
       print("---------------------")
+      belts[belt.a.text.lower()] = "https://www.edge-ma.com/student-area/curriculum/"+belt.a["href"]
+def findlink(belts):
+  what = input("What belt do you want?:    ")
+  try:
+    print(belts[what.lower()])
+    print("")
+    tryagain = input("again y/n:  ")
+    if tryagain == "y":
+        findlink(belts)
+  except:
+    print("belt not found")
+    print( "try again")
+    findlink(belts)
+findlink(belts)
