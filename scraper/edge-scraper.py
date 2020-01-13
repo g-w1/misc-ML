fe0db823863fa18b4c9c8f9aef52c84ec88f507b
g-w1 <58830309+g-w1@users.noreply.github.com>
2020-01-13 10:52:07 -0500

    maybe working

--- scraper/edge-scraper.py
+++ scraper/edge-scraper.py
@@ -1,10 +1,9 @@
 from bs4 import BeautifulSoup
 import requests
 post = 'https://www.edge-ma.com/wp-login.php?action=postpass'
-after = 'https://www.edge-ma.com/student-area/'
+after = 'https://www.edge-ma.com/student-area/curriculum/'
 payload = {"post-password":"DobbsFerry"}
 with requests.Session() as session:
     post = session.post(post, data=payload)
     r = session.get(after)
-    text = BeautifulSoup(r)
-    print(r.text)
+    soup = BeautifulSoup(r.content, 'html.parser')
