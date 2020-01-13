cf300d8e37e9a46728b1e21d9a38b32c5cfe893e
g-w1 <58830309+g-w1@users.noreply.github.com>
2020-01-13 12:24:34 -0500

    Update edge-scraper.py

--- scraper/edge-scraper.py
+++ scraper/edge-scraper.py
@@ -1,19 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
 import webbrowser
-post = 'https://www.edge-ma.com/wp-login.php?action=postpass'
-after = 'https://www.edge-ma.com/student-area/curriculum/'
-payload = {"post_password":"DobbsFerry","Submit":"Enter"}
-with requests.Session() as session:
-    post = session.post(post, data=payload)
-    r = session.get(after)
-    soup = BeautifulSoup(r.content, 'html.parser')
-    a = soup.find_all("div",class_ = "btn-align-center")
-    belts = {}
-    for belt in a:
-      print(belt.a.text.lower())
-      print("---------------------")
-      belts[belt.a.text.lower()] = "https://www.edge-ma.com/student-area/curriculum/"+belt.a["href"]
 def findlink(belts):
   cont = True
   what = input("What belt do you want?: ")
@@ -31,4 +18,24 @@ def findlink(belts):
     print("belt not found")
     print( "try again")
     findlink(belts)
+def urlify(string):
+  
+  a = string.replace(".","")
+  a = a.replace(" ","-")
+  return a
+post = 'https://www.edge-ma.com/wp-login.php?action=postpass'
+after = 'https://www.edge-ma.com/student-area/curriculum/'
+payload = {"post_password":"DobbsFerry","Submit":"Enter"}
+with requests.Session() as session:
+    post = session.post(post, data=payload)
+    r = session.get(after)
+    soup = BeautifulSoup(r.content, 'html.parser')
+    a = soup.find_all("div",class_ = "btn-align-center")
+    belts = {}
+    for belt in a:
+      belts[belt.a.text.lower()] = "https://www.edge-ma.com/student-area/curriculum/"+urlify(belt.a.text.lower())
+      print(belts[belt.a.text.lower()])
+      print(belt.a.text.lower())
+      print("---------------------")
+
 findlink(belts)
