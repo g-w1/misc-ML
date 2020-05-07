3095b160f49787f1c7fa546097733b68279f3a8e
jacob <jacoblevgw@gmail.com>
2020-05-07 13:09:45 -0400

    edit

--- scraper2/edge-scraper.py
+++ scraper2/edge-scraper.py
@@ -12,10 +12,20 @@ with requests.Session() as session:
     r = session.get(after)
     soup = BeautifulSoup(r.content, 'html.parser')
     pages= []
+    subpages={}
+    subsubpages=[]
     for i in soup.find_all("a",attrs={"class":"btn btn-primary"}):
         a =(i['href'])
         pages.append(BeautifulSoup(session.get("http://edgemartialarts.sparkuniversity.co/"+ a).content, 'html.parser'))
     for page in pages:
         links = (page.find_all("tr"))
         for i in links:
-            print(i)
+            try:
+                print(i.td.td)
+                subpages[i.td.td.div.text] = (i['onclick'][9:][:-2])
+            except:
+
+                pass
+    print(subpages)
+    # for a in subpages:
+        # subsubpages.append(BeautifulSoup(session.get("http://edgemartialarts.sparkuniversity.co/"+ a).content, 'html.parser'))
